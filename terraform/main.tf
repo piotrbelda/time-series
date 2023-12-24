882cdb34a67ee57a9bb97b82594cff80268b6430
Piotr Bełda <piotrek.belda@wp.pl>
2023-12-24 11:01:32 +0100

    terraform init

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.10.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "time-series-pb"
+  region  = "europe-central2-a"
+}
+
+resource "google_storage_bucket" "test-bucket" {
+  name          = "time-series-test-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
